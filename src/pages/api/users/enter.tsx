0413a1039f796bb40e84240c1801c2c9aae9b2f9
Bori-github <qhflrnfl4324@gmail.com>
2022-12-22 14:04:53 +0900

    feat: Token 로직 구현

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -4,18 +4,26 @@ import client from 'libs/server/client';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, phone } = req.body;
-  const payload = email ? { email } : { phone: +phone };
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
+  const user = email ? { email } : { phone: +phone };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: 'Anonymous',
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: 'Anonymous',
-      ...payload,
-    },
-    update: {},
   });
+  console.log(token);
 
   return res.status(200).end();
 };
