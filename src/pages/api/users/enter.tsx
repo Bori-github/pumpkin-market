cfed9da00301248a8c3cb1c9ec840fe9f526a3f6
Bori-github <qhflrnfl4324@gmail.com>
2022-12-25 11:12:26 +0900

    feat: handeler, withHandeler에 twilioClient 설정

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,11 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from 'libs/server/withHandler';
+import withHandler, { ResponseType } from 'libs/server/withHandler';
 import client from 'libs/server/client';
+import twilio from 'twilio';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) => {
   const { email, phone } = req.body;
   const payload = Math.floor(100000 + Math.random() * 900000) + '';
-  const user = email ? { email } : { phone: +phone };
+  const user = email ? { email } : phone ? { phone: +phone } : null;
+
+  if (!user) return res.status(400).json({ ok: false });
 
   const token = await client.token.create({
     data: {
@@ -23,9 +31,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     },
   });
-  console.log(token);
 
-  return res.status(200).end();
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.TWILIO_PHONENUMBER!,
+      body: `로그인 토큰은 ${payload} 입니다.`,
+    });
+
+    console.log(message);
+  }
+
+  return res.status(200).json({ ok: true });
 };
 
 export default withHandler('POST', handler);
