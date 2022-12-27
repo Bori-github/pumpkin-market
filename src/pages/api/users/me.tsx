40999578c3198825caf41a294a736fdff4896587
Bori-github <qhflrnfl4324@gmail.com>
2022-12-27 13:42:44 +0900

    feat: 사용자가 로그인했을 경우 사용자 profile get api 생성

---
+++ src/pages/api/users/me.tsx
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from 'libs/server/withHandler';
+import client from 'libs/server/client';
+import { withApiSession } from 'libs/server/withSession';
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) => {
+  const profile = await client.user.findUnique({
+    where: {
+      id: req.session.user?.id,
+    },
+  });
+
+  return res.status(200).json({
+    ok: true,
+    profile,
+  });
+};
+
+export default withApiSession(withHandler('GET', handler));
