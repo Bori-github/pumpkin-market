1a68444fe51710964fd5f1d46a1dc74cc54e6f96
Bori-github <qhflrnfl4324@gmail.com>
2022-12-27 13:41:49 +0900

    feat(api/users/confirm.tsx): withSession 적용

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -1,15 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from 'libs/server/withHandler';
 import client from 'libs/server/client';
+import { withApiSession } from 'libs/server/withSession';
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
   const { token } = req.body;
-  console.log(token);
+  const getToken = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
 
-  return res.status(200).end();
+  if (!getToken) return res.status(404).json({ ok: false });
+
+  req.session.user = {
+    id: getToken.userId,
+  };
+  await req.session.save();
+  await client.token.deleteMany({
+    where: {
+      userId: getToken.userId,
+    },
+  });
+
+  return res.status(200).json({ ok: true });
 };
 
-export default withHandler('POST', handler);
+export default withApiSession(withHandler('POST', handler));
