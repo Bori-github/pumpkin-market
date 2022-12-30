8cdbaf537ec7e1333044c57f0a2cd91a8d996529
Bori-github <qhflrnfl4324@gmail.com>
2022-12-30 16:12:53 +0900

    feat: hot load로 인한 PrismaClient 인스턴스 초기화 방지

--- src/libs/server/client.ts
+++ src/libs/server/client.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient();
+const globalClient = global as unknown as { client: PrismaClient };
+
+const client = globalClient.client || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') globalClient.client = client;
 
 export default client;
